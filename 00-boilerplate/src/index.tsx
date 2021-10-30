3ec2f491059159fe866d3206b887ab62be16886f
manudous <you@example.com>
2021-10-30 23:45:42 +0200

    fix change component name MyComponent to App to work 01-use-state

--- 00-boilerplate/src/index.tsx
+++ 00-boilerplate/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { MyComponent } from "./app";
+import { App } from "./app";
 
 ReactDOM.render(
   <div>
-    <MyComponent />
+    <App />
   </div>,
   document.getElementById("root")
 );
