72539c3e6419c7a54969518dc60535adb487ce17
manudous <you@example.com>
2021-11-03 23:24:11 +0100

    update 06-ajax-field-change

--- 06-ajax-field-change/src/demo.tsx
+++ 06-ajax-field-change/src/demo.tsx
@@ -1,19 +1,21 @@
 import React from "react";
+import { useDebounce } from 'use-debounce';
 
 export const MyComponent = () => {
   const [filter, setFilter] = React.useState("");
+  const [debouncedFilter] =   useDebounce(filter, 500);
   const [userCollection, setUserCollection] = React.useState([]);
 
   // Load full list when the component gets mounted and filter gets updated
   React.useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users?name_like=${filter}`)
-      .then(response => response.json())
-      .then(json => setUserCollection(json));
-  }, [filter]);
+    fetch(`https://swapi.dev/api/people?search=${filter}`)
+      .then((response) => response.json())
+      .then((json) => setUserCollection(json.results));
+  }, [debouncedFilter]);
 
   return (
     <div>
-      <input value={filter} onChange={e => setFilter(e.target.value)} />
+      <input value={filter} onChange={(e) => setFilter(e.target.value)} />
       <ul>
         {userCollection.map((user, index) => (
           <li key={index}>{user.name}</li>
